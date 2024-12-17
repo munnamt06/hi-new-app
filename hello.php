ba8632ac4ab440b4c409edf66c8b00d35bd9c8c4
munnamt06 <munnamt06@gmail.com>
2024-12-17 16:27:42 +0600

    Update hello.php

--- hello.php
+++ hello.php
@@ -2,4 +2,12 @@
 
 echo "Munna";
 
->
+$con = mysqli_connect("localhost","if0_37932748","GjzRVJ1JbkRk7vP","if0_37932748_my_database");
+
+if(mysqli_connect_errno()){
+	echo "Couldn connect to Database !<br>" .mysqli_connect_errno();
+}
+else
+	echo "Munna";
+
+?>
